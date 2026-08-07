21ae66ecabc80ef51a3797cbb6c367fe575f3826
Kopse <leclercq.erwan@protonmail.com>
2026-08-07 18:44:04 +0200

    Adding new configuration builder

--- src/main/java/com/yesman/epicparcool/EpicParCool.java
+++ src/main/java/com/yesman/epicparcool/EpicParCool.java
@@ -1,5 +1,7 @@
 package com.yesman.epicparcool;
 
+import java.io.ObjectInputFilter.Config;
+
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -10,6 +12,8 @@ import com.yesman.epicparcool.event.ParCoolEvents;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLConstructModEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import yesman.epicfight.api.animation.LivingMotion;
@@ -22,22 +26,25 @@ public class EpicParCool {
 	public static final Logger LOGGER = LogUtils.getLogger();
 	public static final String MODID = "epicparcool";
 	public static final String LEAST_PARCOOL_VERSION = "3.4.0.6";
-	
+
 	public EpicParCool(IEventBus modEventBus, ModContainer modContainer) {
 		modEventBus.addListener(ParCoolEvents::onSetup);
 		modEventBus.addListener(this::constructMod);
-		
+
 		if (EpicFightSharedConstants.isPhysicalClient()) {
 			modEventBus.addListener(ParCoolClientEvents::onSetup);
-            modContainer.registerExtensionPoint(IConfigScreenFactory.class, EpicParCoolConfigurations::new);
+			modContainer.registerExtensionPoint(IConfigScreenFactory.class, EpicParCoolConfigurations::new);
 
-            EpicFightEventHooks.Player.CAST_SKILL.registerEvent(ParCoolClientEvents::skillExecute);
-            EpicFightClientEventHooks.Entity.MODIFY_PLAYER_LIVING_MOTION_BASE.registerEvent(ParCoolClientEvents::onBaseLayerUpdateEvent);
+			EpicFightEventHooks.Player.CAST_SKILL.registerEvent(ParCoolClientEvents::skillExecute);
+			EpicFightClientEventHooks.Entity.MODIFY_PLAYER_LIVING_MOTION_BASE
+					.registerEvent(ParCoolClientEvents::onBaseLayerUpdateEvent);
+
 		}
+		modContainer.registerConfig(ModConfig.Type.CLIENT, EpicParCoolConfigurations.SPEC);
+		EpicFightEventHooks.Animation.INIT_ANIMATOR.registerEvent(ParCoolEvents::onInitAnimatorEvent);
 
-        EpicFightEventHooks.Animation.INIT_ANIMATOR.registerEvent(ParCoolEvents::onInitAnimatorEvent);
 	}
-	
+
 	public void constructMod(FMLConstructModEvent event) {
 		LivingMotion.ENUM_MANAGER.registerEnumCls(EpicParCool.MODID, ParcoolLivingMotions.class);
 	}
--- src/main/java/com/yesman/epicparcool/client/screen/EpicParCoolConfigurations.java
+++ src/main/java/com/yesman/epicparcool/client/screen/EpicParCoolConfigurations.java
@@ -17,6 +17,8 @@ import net.minecraft.util.StringRepresentable;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.fml.ModContainer;
+import net.neoforged.neoforge.common.ModConfigSpec;
+
 import org.jetbrains.annotations.Nullable;
 import yesman.epicfight.api.utils.ParseUtil;
 import yesman.epicfight.client.gui.datapack.screen.MessageScreen;
@@ -35,10 +37,10 @@ import static yesman.epicfight.generated.LangKeys.*;
 
 @OnlyIn(Dist.CLIENT)
 public class EpicParCoolConfigurations extends Screen {
-	private static final StaminaType[] STAMINA_TYPE_ENUMS = StaminaType.values();
+    private static final StaminaType[] STAMINA_TYPE_ENUMS = StaminaType.values();
 
     @Nullable
-	private final Screen parentScreen;
+    private final Screen parentScreen;
     private final AnchoredButton saveButton;
     private final AnchoredButton discardButton;
 
@@ -47,125 +49,167 @@ public class EpicParCoolConfigurations extends Screen {
     @Nullable
     protected SettingTitle hoveringSettingTitle = null;
 
-	public EpicParCoolConfigurations(@Nullable final ModContainer mod, @Nullable Screen screen) {
-		super(Component.translatable("gui.epicparcool.config_screen.title"));
-		
-		this.parentScreen = screen;
+
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+    private static ModConfigSpec.BooleanValue IS_WEAPON_TO_BACK = BUILDER.define("isWeaponToBack", true);
+    public static final ModConfigSpec SPEC = BUILDER.build();
+    private static final Boolean[] aList = {true, false};
+
+    public EpicParCoolConfigurations(@Nullable final ModContainer mod, @Nullable Screen screen) {
+        super(Component.translatable("gui.epicparcool.config_screen.title"));
+
+        this.parentScreen = screen;
         this.minecraft = screen == null ? Minecraft.getInstance() : screen.getMinecraft();
 
         this.saveButton = AnchoredButton.buttonBuilder(
-            Component.translatable(GUI_WIDGET_COMMON_SAVE),
-            button -> {
-                List<Runnable> toSave = new ArrayList<>();
-                List<Runnable> toDiscard = new ArrayList<>();
-                ClientConfig.checkUnsaved(toSave, toDiscard);
+                Component.translatable(GUI_WIDGET_COMMON_SAVE),
+                button -> {
+                    List<Runnable> toSave = new ArrayList<>();
+                    List<Runnable> toDiscard = new ArrayList<>();
+                    ClientConfig.checkUnsaved(toSave, toDiscard);
 
-                for (Runnable runnable : toSave) {
-                    runnable.run();
-                }
+                    for (Runnable runnable : toSave) {
+                        runnable.run();
+                    }
 
-                this.minecraft.setScreen(this.parentScreen);
-            }
-        )
-        .xParams(115, 80)
-        .yParams(10, 20)
-        .horizontalAnchorType(AnchoredWidget.HorizontalAnchorType.RIGHT_WIDTH)
-        .verticalAnchorType(AnchoredWidget.VerticalAnchorType.BOTTOM_HEGIHT)
-        .theme(AnchoredButton.BuiltInTheme.BLACK)
-        .highlihgtFontWhen(AbstractWidget::isHoveredOrFocused)
-        .alpha(0.6F)
-        .build();
+                    this.minecraft.setScreen(this.parentScreen);
+                })
+                .xParams(115, 80)
+                .yParams(10, 20)
+                .horizontalAnchorType(AnchoredWidget.HorizontalAnchorType.RIGHT_WIDTH)
+                .verticalAnchorType(AnchoredWidget.VerticalAnchorType.BOTTOM_HEGIHT)
+                .theme(AnchoredButton.BuiltInTheme.BLACK)
+                .highlihgtFontWhen(AbstractWidget::isHoveredOrFocused)
+                .alpha(0.6F)
+                .build();
 
         this.discardButton = AnchoredButton.buttonBuilder(
-            Component.translatable(GUI_WIDGET_COMMON_DISCARD),
-            button -> {
-                List<Runnable> toSave = new ArrayList<>();
-                List<Runnable> toDiscard = new ArrayList<> ();
-                ClientConfig.checkUnsaved(toSave, toDiscard);
+                Component.translatable(GUI_WIDGET_COMMON_DISCARD),
+                button -> {
+                    List<Runnable> toSave = new ArrayList<>();
+                    List<Runnable> toDiscard = new ArrayList<>();
+                    ClientConfig.checkUnsaved(toSave, toDiscard);
 
-                if (!toSave.isEmpty() && !toDiscard.isEmpty()) {
-                    this.minecraft.setScreen(new MessageScreen<>(
-                        "",
-                        Component.translatable(GUI_MESSAGE_SETTINGS_DISCARD_CHANGES_NOTIFICATION),
-                        this,
-                        button$2 -> {
-                            toDiscard.forEach(Runnable::run);
-                            this.minecraft.setScreen(this.parentScreen);
-                        },
-                        button$2 -> {
-                            this.minecraft.setScreen(this);
-                        },
-                        180,
-                        0
-                    ).autoCalculateHeight());
-                } else {
-                    this.minecraft.setScreen(this.parentScreen);
-                }
-            }
-        )
-        .xParams(10, 100)
-        .yParams(10, 20)
-        .horizontalAnchorType(AnchoredWidget.HorizontalAnchorType.RIGHT_WIDTH)
-        .verticalAnchorType(AnchoredWidget.VerticalAnchorType.BOTTOM_HEGIHT)
-        .theme(AnchoredButton.BuiltInTheme.BLACK)
-        .highlihgtFontWhen(AbstractWidget::isHoveredOrFocused)
-        .alpha(0.6F)
-        .build();
+                    if (!toSave.isEmpty() && !toDiscard.isEmpty()) {
+                        this.minecraft.setScreen(new MessageScreen<>(
+                                "",
+                                Component.translatable(GUI_MESSAGE_SETTINGS_DISCARD_CHANGES_NOTIFICATION),
+                                this,
+                                button$2 -> {
+                                    toDiscard.forEach(Runnable::run);
+                                    this.minecraft.setScreen(this.parentScreen);
+                                },
+                                button$2 -> {
+                                    this.minecraft.setScreen(this);
+                                },
+                                180,
+                                0).autoCalculateHeight());
+                    } else {
+                        this.minecraft.setScreen(this.parentScreen);
+                    }
+                })
+                .xParams(10, 100)
+                .yParams(10, 20)
+                .horizontalAnchorType(AnchoredWidget.HorizontalAnchorType.RIGHT_WIDTH)
+                .verticalAnchorType(AnchoredWidget.VerticalAnchorType.BOTTOM_HEGIHT)
+                .theme(AnchoredButton.BuiltInTheme.BLACK)
+                .highlihgtFontWhen(AbstractWidget::isHoveredOrFocused)
+                .alpha(0.6F)
+                .build();
 
-        widgetTable = new WidgetTable(this, 10, 0, 5, 10, AnchoredWidget.HorizontalAnchorType.LEFT_RIGHT, AnchoredWidget.VerticalAnchorType.TOP_BOTTOM, 21);
-        textBox = new TextBox(this.minecraft.font, 10, 0, 5, 40, AnchoredWidget.HorizontalAnchorType.RIGHT_WIDTH, AnchoredWidget.VerticalAnchorType.TOP_BOTTOM, Component.empty());
+        widgetTable = new WidgetTable(this, 10, 0, 5, 10, AnchoredWidget.HorizontalAnchorType.LEFT_RIGHT,
+                AnchoredWidget.VerticalAnchorType.TOP_BOTTOM, 21);
+        textBox = new TextBox(this.minecraft.font, 10, 0, 5, 40, AnchoredWidget.HorizontalAnchorType.RIGHT_WIDTH,
+                AnchoredWidget.VerticalAnchorType.TOP_BOTTOM, Component.empty());
 
         System.out.println(ParCoolConfig.Client.getInstance().StaminaType.get());
 
         widgetTable
-            .newRow()
+                .newRow()
                 .addWidget(
-                    new SettingTitle(
-                        minecraft.font,
-                        this.widgetTable.nextX(4),
-                        125,
-                        0,
-                        15,
-                        AnchoredWidget.HorizontalAnchorType.LEFT_RIGHT,
-                        AnchoredWidget.VerticalAnchorType.TOP_HEIGHT,
-                        "gui.epicparcool.widget.config_screen.stamina_source",
-                        this.textBox::setMessage,
-                        this::getHoveringSettingTitle,
-                        this::setHoveringSettingTitle,
-                        this::getFocusedWidgetFromTable
-                    )
-                )
+                        new SettingTitle(
+                                minecraft.font,
+                                this.widgetTable.nextX(4),
+                                125,
+                                0,
+                                15,
+                                AnchoredWidget.HorizontalAnchorType.LEFT_RIGHT,
+                                AnchoredWidget.VerticalAnchorType.TOP_HEIGHT,
+                                "gui.epicparcool.widget.config_screen.stamina_source",
+                                this.textBox::setMessage,
+                                this::getHoveringSettingTitle,
+                                this::setHoveringSettingTitle,
+                                this::getFocusedWidgetFromTable))
                 .addWidget(
-                    new ComboBox<>(
-                        this,
-                        minecraft.font,
-                        10,
-                        100,
-                        0,
-                        15,
-                        AnchoredWidget.HorizontalAnchorType.RIGHT_WIDTH,
-                        AnchoredWidget.VerticalAnchorType.TOP_HEIGHT,
-                        () -> ParCoolConfig.Client.getInstance().StaminaType.get(),
-                        value -> ParCoolConfig.Client.getInstance().StaminaType.set(value),
-                        8,
-                        Component.translatable("gui.epicparcool.widget.config_screen.stamina_source"),
-                        List.of(StaminaType.values()),
-                        value -> Component.translatable("gui.epicparcool.widget.stamina_source." + ParseUtil.toLowerCase(value.name())).getString()
-                    )
-                );
+                        new ComboBox<>(
+                                this,
+                                minecraft.font,
+                                10,
+                                100,
+                                0,
+                                15,
+                                AnchoredWidget.HorizontalAnchorType.RIGHT_WIDTH,
+                                AnchoredWidget.VerticalAnchorType.TOP_HEIGHT,
+                                () -> ParCoolConfig.Client.getInstance().StaminaType.get(),
+                                value -> ParCoolConfig.Client.getInstance().StaminaType.set(value),
+                                8,
+                                Component.translatable("gui.epicparcool.widget.config_screen.stamina_source"),
+                                List.of(StaminaType.values()),
+                                value -> Component.translatable(
+                                        "gui.epicparcool.widget.stamina_source." + ParseUtil.toLowerCase(value.name()))
+                                        .getString()));
+
+        widgetTable.newRow().addWidget(new SettingTitle(
+                minecraft.font,
+                this.widgetTable.nextX(4),
+                125,
+                0,
+                15,
+                AnchoredWidget.HorizontalAnchorType.LEFT_RIGHT,
+                AnchoredWidget.VerticalAnchorType.TOP_HEIGHT,
+                "gui.epicparcool.widget.config_screen.weapon_to_back",
+                this.textBox::setMessage,
+                this::getHoveringSettingTitle,
+                this::setHoveringSettingTitle,
+                this::getFocusedWidgetFromTable))
+                .addWidget(
+                        new ComboBox<>(
+                                this,
+                                minecraft.font,
+                                10,
+                                100,
+                                0,
+                                15,
+                                AnchoredWidget.HorizontalAnchorType.RIGHT_WIDTH,
+                                AnchoredWidget.VerticalAnchorType.TOP_HEIGHT,
+                                () -> (boolean) IS_WEAPON_TO_BACK.get(),
+                                value -> setIsWeaponToBack((boolean)value),
+                                2,
+                                Component.translatable("gui.epicparcool.widget.config_screen.weapon_to_back"),
+                                List.of(aList),
+                                value -> Component
+                                        .translatable(
+                                                "gui.epicparcool.widget.weapon_to_back."
+                                                        + ParseUtil.toLowerCase(value.toString()))
+                                        .getString()));
+        ;
 
         this.widgetTable.initialize(false);
-	}
-	
-	@Override
-	protected void init() {
+    }
+
+    private void setIsWeaponToBack(boolean value) {
+            IS_WEAPON_TO_BACK = BUILDER.define("is_weapon_to_back", value);
+    }
+
+    @Override
+    protected void init() {
         this.addRenderableWidget(this.saveButton);
         this.addRenderableWidget(this.discardButton);
         this.addRenderableWidget(this.widgetTable);
         this.addRenderableWidget(this.textBox);
 
         this.repositionElements();
-	}
+    }
 
     @Override
     public void repositionElements() {
@@ -178,16 +222,17 @@ public class EpicParCoolConfigurations extends Screen {
         this.discardButton.relocate(rectangle);
     }
 
-	@Override
-	public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
-		Component warning = Component.translatable("gui.epicparcool.config_screen.warn");
-		super.render(guiGraphics, mouseX, mouseY, partialTicks);
-		guiGraphics.drawString(this.font, warning, 6, this.height - 50, 16777215);
-	}
+    @Override
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
+        Component warning = Component.translatable("gui.epicparcool.config_screen.warn");
+        super.render(guiGraphics, mouseX, mouseY, partialTicks);
+        guiGraphics.drawString(this.font, warning, 6, this.height - 50, 16777215);
+    }
 
     @Nullable
     public GuiEventListener getFocusedWidgetFromTable() {
-        if (this.getFocused() == this.widgetTable && this.widgetTable.getSelected() instanceof WidgetTable.WidgetEntry widgetEntry) {
+        if (this.getFocused() == this.widgetTable
+                && this.widgetTable.getSelected() instanceof WidgetTable.WidgetEntry widgetEntry) {
             return widgetEntry.getFocused();
         }
 
@@ -203,29 +248,28 @@ public class EpicParCoolConfigurations extends Screen {
         this.hoveringSettingTitle = settingTitle;
     }
 
-	@Override
-	public void onClose() {
+    @Override
+    public void onClose() {
         List<Runnable> toSave = new ArrayList<>();
         List<Runnable> toDiscard = new ArrayList<>();
         ClientConfig.checkUnsaved(toSave, toDiscard);
 
         if (!toSave.isEmpty() && !toDiscard.isEmpty()) {
-            this.minecraft.setScreen(new MessageScreen<> (
-                "",
-                Component.translatable(GUI_MESSAGE_SETTINGS_UNSAVED_CHANGES_NOTIFICATION),
-                this,
-                button -> {
-                    toDiscard.forEach(Runnable::run);
-                    this.minecraft.setScreen(this.parentScreen);
-                },
-                button -> {
-                    this.minecraft.setScreen(this);
-                },
-                180,
-                0
-            ).autoCalculateHeight());
+            this.minecraft.setScreen(new MessageScreen<>(
+                    "",
+                    Component.translatable(GUI_MESSAGE_SETTINGS_UNSAVED_CHANGES_NOTIFICATION),
+                    this,
+                    button -> {
+                        toDiscard.forEach(Runnable::run);
+                        this.minecraft.setScreen(this.parentScreen);
+                    },
+                    button -> {
+                        this.minecraft.setScreen(this);
+                    },
+                    180,
+                    0).autoCalculateHeight());
         } else {
             this.minecraft.setScreen(this.parentScreen);
         }
-	}
+    }
 }
